f0a89a2bc4144840568bc8bf1c7d2cb78959125e
yrakhwas <yrakhwas@gmail.com>
2025-05-28 15:58:58 +0300

    Add change style button

--- main.js
+++ main.js
@@ -6,7 +6,38 @@ const tableBody = document.getElementById("table-body");
 const createBtn = document.getElementById("create-btn");
 const deleteBtn = document.getElementById("delete-btn");
 
+const themeBtn = document.getElementById("theme-btn");
+
+const nav = document.getElementById("navbar");
+
+
+
+
+
 let products = [];
+
+themeBtn.onclick = (event) =>{
+    event.preventDefault();
+    console.log("Theme button clicked");
+   if(themeBtn.innerHTML == "Light") 
+   {
+    themeBtn.innerHTML = "Dark";
+    nav.classList.remove("bg-light");
+
+    nav.classList.add("bg-dark");
+
+   }
+   else
+   {
+    themeBtn.innerHTML = "Light";
+    nav.classList.remove("bg-dark");
+    nav.classList.add("bg-light");
+   }
+    
+}
+
+
+
 
 deleteBtn.onclick = ()=>
 {
@@ -14,17 +45,15 @@ deleteBtn.onclick = ()=>
     tableBody.innerHTML = "";
 }
 
-createBtn.onclick = () =>{
-    
-    const newProduct = getProductFromForm();
-
-    products.push(newProduct);
-
-
-    addProductToTable(newProduct);
-    
+function addProductToTable(product)
+{
+    tableBody.innerHTML += `<tr class="table-light">
+                            <th scope="row">${product.id}</th>
+                            <td>${product.model}</td>
+                            <td>${product.description}</td>
+                            <td>${product.price}$</td>
+                        </tr>`;
 }
-
 
 function getProductFromForm(){
     const modelInput =  document.getElementById("modelInput");
@@ -39,13 +68,18 @@ function getProductFromForm(){
     }
 
 }
+createBtn.onclick = (event) =>{
+    event.preventDefault();
+    console.log("Create button clicked");
+    const newProduct = getProductFromForm();
 
-function addProductToTable(product)
-{
-    tableBody.innerHTML += `<tr class="table-light">
-                            <th scope="row">${product.id}</th>
-                            <td>${product.model}</td>
-                            <td>${product.description}</td>
-                            <td>${product.price}$</td>
-                        </tr>`;
+    products.push(newProduct);
+
+
+    addProductToTable(newProduct);
+    console.log("products", products);
 }
+
+
+
+
